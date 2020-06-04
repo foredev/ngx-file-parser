97c76f577cd8a33f0940f855f4e18cbd2391c91a
Rasmus Brants <rasmusbrants@gmail.com>
2020-06-04 15:35:44 +0200

    ADD comments to config

--- projects/ngx-file-parser/src/lib/interfaces/config.model.ts
+++ projects/ngx-file-parser/src/lib/interfaces/config.model.ts
@@ -1,7 +1,24 @@
 export class NgxFileButtonConfig {
+  /**
+   * Text to be displayed on the main button
+   * Defaults to 'Choose files'
+   */
   text?: string;
+  /**
+   * Material icon to be displayed on the main button
+   * Defaults to 'backup'
+   */
   icon?: string;
+  /**
+   * File name extensions to be accepted to process
+   * Supported file extensions: .csv
+   * Defaults to .csv
+   */
   accepts: SUPPORTED_FILES[];
+  /**
+   * If user should be have to ability to choose multiple files
+   *Defaults to false
+   */
   multiple?: boolean;
 }
 type SUPPORTED_FILES = '.csv';
